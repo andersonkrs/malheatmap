5bbe9a4e15b137a0eecfc6ec5ba8ebdacf99650c
Anderson Macedo <anderson.krs95@gmail.com>
2020-03-08 10:05:58 +0000

    Increase capybara timeout

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,8 +2,10 @@ ENV["COVERAGE"] = "false"
 
 require "test_helper"
 require "webdrivers"
+require "capybara"
 
 Webdrivers::Chromedriver.update
+Capybara.default_max_wait_time = 20
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome
