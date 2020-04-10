3ba13ed185bffa50fe39925ed0637e9c572b9583
Anderson Macedo <anderson.krs95@gmail.com>
2020-04-10 16:40:25 +0000

    Fix activities order

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :entries, dependent: :destroy
-  has_many :activities, dependent: :destroy
+  has_many :activities, -> { order(date: :desc) }, dependent: :destroy
 
   def to_param
     username
