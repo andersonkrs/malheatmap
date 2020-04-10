6253c07bdd823d680561a8229dae5b8c618b23c4
Anderson Macedo <anderson.krs95@gmail.com>
2020-04-10 16:18:35 +0000

    Fix coverage

--- test/lib/mal/urls_test.rb
+++ test/lib/mal/urls_test.rb
@@ -4,11 +4,11 @@ class URLSTest < ActiveSupport::TestCase
   include MAL::URLS
 
   test "builds anime url with given id" do
-    assert_equal item_url(:anime, 2), "https://myanimelist.net/anime/2"
+    assert_equal anime_url(2), "https://myanimelist.net/anime/2"
   end
 
   test "builds manga url with given id" do
-    assert_equal item_url(:manga, 1), "https://myanimelist.net/manga/1"
+    assert_equal manga_url(1), "https://myanimelist.net/manga/1"
   end
 
   test "builds user profile URL with given username" do
