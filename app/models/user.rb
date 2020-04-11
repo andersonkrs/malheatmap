46870ca8e1883ff1bbb4da88751683e53e55281a
Anderson Macedo <anderson.krs95@gmail.com>
2020-04-11 02:33:22 +0000

    Fix offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :entries, dependent: :destroy
-  has_many :activities, -> { order(date: :desc) }, dependent: :destroy
+  has_many :activities, -> { order(date: :desc) }, dependent: :destroy, inverse_of: :user
 
   def to_param
     username
