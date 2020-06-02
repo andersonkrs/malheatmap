44e7d75eda87c7586035bfa6a4aad29a7318c6dc
Anderson Macedo <anderson.krs95@gmail.com>
2020-06-02 00:49:11 -0300

    Capture browser logs on system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,9 +4,32 @@ require "test_helper"
 require "webdrivers"
 require "capybara"
 
-Webdrivers::Chromedriver.update
 Capybara.default_max_wait_time = 20
 
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(
+    args: %w[headless no-sandbox window-size=1920,1080]
+  )
+  Capybara::Selenium::Driver.new(app, browser: :chrome,
+                                      options: options,
+                                      desired_capabilities: {
+                                        "goog:loggingPrefs" => { browser: "ALL" }
+                                      })
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome
+  driven_by :headless_chrome
+
+  def before_teardown
+    super
+    flush_browser_logs
+  end
+
+  private
+
+  def flush_browser_logs
+    page.driver.browser.manage.logs.get("browser").each do |entry|
+      Rails.logger.info "[Browser] #{entry}"
+    end
+  end
 end
