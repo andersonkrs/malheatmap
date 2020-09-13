2f2ba0fe60a6dc40019026ea210c4cd368db56f7
Anderson Macedo <anderson.krs95@gmail.com>
2020-09-13 11:08:42 -0300

    Improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def meta_description(text)
+    content_for(:meta_description, text)
+  end
+
+  def page_title(text)
+    content_for(:title, text)
+  end
+
+  def section_size(size)
+    content_for(:section_class, "is-#{size}")
+  end
 end
