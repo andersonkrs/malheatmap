f93e4b123919534be14db00eaf04f035a4868152
Anderson Macedo <anderson.krs95@gmail.com>
2020-09-13 12:56:30 -0300

    Fix error

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -8,7 +8,7 @@ class SubscriptionsController < ApplicationController
 
     if @subscription.invalid?
       flash[:error] = @subscription.errors.full_messages.first
-      return redirect_to_index
+      return redirect_to(action: :index)
     end
 
     if @subscription.user_already_subscribed?
@@ -26,9 +26,5 @@ class SubscriptionsController < ApplicationController
 
   def redirect_to_user_page
     redirect_to(user_path(@subscription.username), turbolinks: :advance)
-  end
-
-  def redirect_to_index
-    redirect_to(action: :index, turbolinks: :advance)
   end
 end
