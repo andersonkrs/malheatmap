70ed08b8da0d66bf3de2e95df68390909b0da90b
Anderson Macedo <anderson.krs95@gmail.com>
2020-09-28 08:42:25 -0300

    Schedule users update with more frequency
    Update all users in one batch can be hard do process, I've changed it to process 100 users each 30m, just users that have not been updated 12 hours at least.

--- app/jobs/schedule_users_data_update_job.rb
+++ app/jobs/schedule_users_data_update_job.rb
@@ -4,8 +4,20 @@ class ScheduleUsersDataUpdateJob < ApplicationJob
   def perform
     Rails.logger.info "Updating users data"
 
-    User.order(:updated_at).each do |user|
+    users = users_have_not_been_updated(at_least: 8.hours)
+    users.each do |user|
       User::UpdateDataJob.set(wait: 5.seconds).perform_later(user)
     end
+  end
+
+  private
+
+  def users_have_not_been_updated(at_least:)
+    User
+      .where(User.arel_table[:updated_at].lteq(at_least.ago))
+      .order(:updated_at)
+      .limit(50)
+      .select(:id)
+      .shuffle
   end
 end
--- test/jobs/schedule_users_data_update_job_test.rb
+++ test/jobs/schedule_users_data_update_job_test.rb
@@ -1,16 +1,22 @@
 require "test_helper"
 
 class ScheduleUsersDataUpdateJobTest < ActiveJob::TestCase
-  setup do
-    @users = create_list(:user, 3)
-  end
+  test "schedules update data job for each user which is qualified for updating based on the last update date" do
+    qualified_users = create_list(:user, 3, updated_at: 8.hours.ago)
+    create_list(:user, 4, updated_at: 3.hours.ago)
+    create_list(:user, 2, updated_at: 2.hours.ago)
 
-  test "schedule update data job for each user on db" do
     ScheduleUsersDataUpdateJob.perform_now
 
     assert_enqueued_jobs 3
-    assert_enqueued_with job: User::UpdateDataJob, args: [@users.first]
-    assert_enqueued_with job: User::UpdateDataJob, args: [@users.second]
-    assert_enqueued_with job: User::UpdateDataJob, args: [@users.third]
+    assert_enqueued_with job: User::UpdateDataJob, args: [qualified_users.first]
+    assert_enqueued_with job: User::UpdateDataJob, args: [qualified_users.second]
+    assert_enqueued_with job: User::UpdateDataJob, args: [qualified_users.third]
+
+    travel_to 9.hours.from_now
+
+    assert_enqueued_jobs 9 do
+      ScheduleUsersDataUpdateJob.perform_now
+    end
   end
 end
