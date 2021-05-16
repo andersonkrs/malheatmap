c6dd6bda07fdacf806c983597f60b1c83e7ad228
Anderson Macedo <anderson.krs95@gmail.com>
2021-05-16 09:15:19 -0300

    Add some docs to the calendar component

--- app/components/calendar_component.rb
+++ app/components/calendar_component.rb
@@ -1,4 +1,32 @@
 class CalendarComponent < ViewComponent::Base
+  # Component that represents the activities calendar
+  # The calendar always start on a Sunday
+  #
+  #              |Jan                |Feb                |Mar                |Apr
+  #        SUN   04   11   18   25   01   08   15   22   29   07   14   21   28   04
+  #        MON   05   12   19   26   02   09   16   23   01   08   15   22   29   05
+  #        TUE   06   13   20   27   03   10   17   24   02   09   16   23   30   06
+  #        WED   07   14   21   28   04   11   18   25   03   10   17   24   31   07
+  #        THU   08   15   22   29   05   12   19   26   04   11   18   25   01
+  #        FRI   09   16   23   30   06   13   20   27   05   12   19   26   02
+  #        SAT   10   17   24   31   07   14   21   28   06   13   20   27   03
+  #
+  # Each square represents a day, each group of 7 squares is a week column.
+  # The months' headers are calculated based on the number of weeks depending how many week columns the month is
+  # present in, the same week column can contain days of following and/or previous months.
+  #
+  # The number of weeks is subtracted by 1 in order to keep the month's name on the exact column that it starts.
+  # Usually a month will start and at the end or in the middle of a week column,
+  # sharing the same week with the previous month and next month.
+  #
+  # If the month ends on a saturday, we dont need to subtract, since the next week will be fully taken by
+  # following month.
+  #
+  # If the month is the last of the date range, we do not subtract, this adds the trailing amount of space to the
+  # last month in order to match the sum of all months' widths with the total of weeks
+  #
+  # width of the month's header = Number of weeks of the month * --week-width in pixes
+
   Month = Struct.new(:month, :year, :weeks, :last, keyword_init: true) do
     def label
       I18n.t("date.abbr_month_names")[month]
@@ -13,7 +41,7 @@ class CalendarComponent < ViewComponent::Base
     end
 
     def width
-      if month_ends_on_the_last_saturday_of_the_last_week? || last_month?
+      if month_ends_on_a_saturday? || last_month?
         weeks.size
       else
         weeks.size - 1
@@ -22,7 +50,7 @@ class CalendarComponent < ViewComponent::Base
 
     private
 
-    def month_ends_on_the_last_saturday_of_the_last_week?
+    def month_ends_on_a_saturday?
       last_week = weeks.last
       last_week_day = last_week.last
       last_month_day = Date.new(year, month, 1).end_of_month
