37bfcaeb02afdb5c6be1e344f351e80db58d546d
Anderson Macedo <anderson.krs95@gmail.com>
2021-07-03 16:20:07 -0300

    Add missing file

---
+++ lib/browser_session.rb
@@ -0,0 +1,46 @@
+class BrowserSession
+  extend ActiveSupport::PerThreadRegistry
+
+  attr_accessor :current
+
+  # Reuses the same browser that is on the thread instead of spawning a browser process each time
+  # Ideally the browser process is spawned once by the threads manager process like Rails/Sidekiq and then it is
+  # reused everytime just create new tabs
+  #
+  # If there's no browser assigned to the thread a new browser process will be spawned and terminated after
+  # performing the given block work on the func #fetch_page
+
+  def self.fetch_page
+    if current.present?
+      page = current.create_page
+      begin
+        yield(page)
+      ensure
+        page.close
+      end
+    else
+      temp_browser = new_browser
+      begin
+        yield(temp_browser.page)
+      ensure
+        temp_browser.quit
+      end
+    end
+  end
+
+  def self.new_browser
+    Ferrum.with_attempts(errors: StandardError, max: 3, wait: 1.second) do
+      Ferrum::Browser.new(
+        headless: !ENV["HEADLESS"].in?(%w[n 0 no false]),
+        browser_options: {
+          "no-sandbox": nil,
+          "disable-setuid-sandbox": nil
+        },
+        timeout: 30,
+        process_timeout: 30
+      ).tap do |browser|
+        Rails.logger.info "Browser instance created PID: #{browser.process.pid}"
+      end
+    end
+  end
+end
