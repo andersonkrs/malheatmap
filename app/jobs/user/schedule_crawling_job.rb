bfa8f8d379ff775c03e9906989a06b106fc31d94
Anderson Macedo <anderson.krs95@gmail.com>
2021-07-02 22:31:01 -0300

    Schedule users crawl data job at once instead of batches

--- app/jobs/user/schedule_crawling_job.rb
+++ app/jobs/user/schedule_crawling_job.rb
@@ -2,14 +2,8 @@ class User
   class ScheduleCrawlingJob < ApplicationJob
     queue_as :default
 
-    def perform(batch_size: 100, batch_interval: 15.minutes)
-      User.in_batches(of: batch_size).each_with_index do |batch, batch_index|
-        interval = batch_interval * (batch_index + 1)
-
-        batch.each do |user|
-          user.crawl_data_later(wait: interval)
-        end
-      end
+    def perform
+      User.find_each(batch_size: 50, &:crawl_data_later)
     end
   end
 end
