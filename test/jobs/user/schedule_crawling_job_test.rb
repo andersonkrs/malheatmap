a762cad4f63368c4acf2079830fc74727e0664d2
Anderson Macedo <anderson.krs95@gmail.com>
2021-07-02 22:31:01 -0300

    Schedule users crawl data job at once instead of batches

--- test/jobs/user/schedule_crawling_job_test.rb
+++ test/jobs/user/schedule_crawling_job_test.rb
@@ -2,17 +2,13 @@ require "test_helper"
 
 class User
   class ScheduleCrawlingJobTest < ActiveJob::TestCase
-    test "schedules the crawling jobs for each user with intervals between each batch" do
-      ScheduleCrawlingJob.perform_now(batch_size: 1)
+    test "schedules the crawling jobs for each user" do
+      ScheduleCrawlingJob.perform_now
 
       assert_enqueued_jobs User.count
       assert_enqueued_with job: User::CrawlDataJob, args: [users(:john_doe)]
       assert_enqueued_with job: User::CrawlDataJob, args: [users(:babyoda)]
       assert_enqueued_with job: User::CrawlDataJob, args: [users(:anderson)]
-
-      assert_enqueued_with job: User::CrawlDataJob, at: 15.minutes.from_now
-      assert_enqueued_with job: User::CrawlDataJob, at: 30.minutes.from_now
-      assert_enqueued_with job: User::CrawlDataJob, at: 45.minutes.from_now
     end
   end
 end
