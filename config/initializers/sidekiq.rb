438ba90f6a7d7d52d658b082714a3bd0a8d892dc
Anderson Macedo <anderson.krs95@gmail.com>
2021-07-24 17:12:18 -0300

    Use shared redis pool for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,5 @@
 Sidekiq.configure_client do |config|
-  config.redis = {
-    size: 2,
-    url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" }
-  }
+  config.redis = Rails.cache.redis
 end
 
 class ThreadedBrowserMiddleware
