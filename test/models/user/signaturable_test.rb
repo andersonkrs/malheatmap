6d738691f2b2f7b68343ee1f9d81e624dbaa66f9
Anderson Macedo <anderson.krs95@gmail.com>
2022-10-30 13:37:28 -0300

    Rubocop

--- test/models/user/signaturable_test.rb
+++ test/models/user/signaturable_test.rb
@@ -7,7 +7,7 @@ class User
     end
 
     test   "generates user signature image correctly using current date"    do
-      travel_to Date.new(2020, 5, 1)
+        travel_to Date.new(2020, 5, 1)
 
       @user.activities.create!(
         [
