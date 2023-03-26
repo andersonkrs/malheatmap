cf6222f287f8d7249eb4fb87b2d10dd98168aa65
Anderson Macedo <anderson.krs95@gmail.com>
2023-03-26 10:44:06 -0300

    Update defaults

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ Bundler.require(*Rails.groups)
 
 module MalHeatmap
   class Application < Rails::Application
-    config.load_defaults 7.0
+    config.load_defaults 7.1
     config.time_zone = "UTC"
     config.autoload_paths << Rails.root.join("lib")
 
