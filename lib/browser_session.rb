c410daf5efb891ca62e073c6102f4cc299124e07
Anderson Macedo <anderson.krs95@gmail.com>
2024-02-17 08:01:55 -0300

    Spawn chrome sessions concurrently

--- lib/browser_session.rb
+++ lib/browser_session.rb
@@ -1,53 +1,23 @@
 class BrowserSession
-  include Singleton
-
-  attr_reader :browser
-  attr_reader :logger
-
-  def initialize
-    super
-    @logger = ActiveSupport::TaggedLogging.new(Rails.logger)
-    @mutex = Mutex.new
-  end
-
   RETRYABLE_ERRORS = [Ferrum::TimeoutError, Ferrum::ProcessTimeoutError, NoMethodError].freeze
 
-  # Reuses the same browser that is on the thread instead of spawning a browser process each time
-  # Ideally the browser process is spawned once and reused during the thread session
-  #
-  # If there's no browser assigned to the thread a new browser process will be spawned and terminated after
-  # performing the given block work on the func #fetch_page
-  def fetch_page(&)
-    mutex.synchronize do
-      spawn_new_browser unless browser.present? && browser.default_context.present?
-    end
-
-    with_new_page(&)
+  def self.fetch_page(&)
+    instance = new
+    instance.with_new_page(&)
+  ensure
+    instance.quit
   end
 
   def quit
     if browser
-      logger.info "Quitting browser process #{browser.process.pid}"
+      Rails.logger.info "Quitting browser process #{browser.process.pid}"
       browser.quit
     end
   end
 
-  private
-
-  attr_reader :mutex
-
-  def spawn_new_browser
-    @browser =
-      Ferrum::Utils::Attempt.with_retry(errors: RETRYABLE_ERRORS, max: 3, wait: 3.seconds) do
-        Ferrum::Browser
-          .new(headless: "new", browser_options: { "no-sandbox": nil, "disable-setuid-sandbox": nil })
-          .tap do |browser|
-            logger.info "Browser instance created PID: #{browser.process.pid}"
-          end
-      end
-  end
-
   def with_new_page
+    spawn_new_browser
+
     Ferrum::Utils::Attempt.with_retry(errors: RETRYABLE_ERRORS, max: 3, wait: 3.seconds) do
       page = browser.create_page
       begin
@@ -57,8 +27,19 @@ class BrowserSession
       end
     end
   end
-end
 
-at_exit do
-  BrowserSession.instance&.quit
+  private
+
+  attr_reader :browser
+
+  def spawn_new_browser
+    @browser =
+      Ferrum::Utils::Attempt.with_retry(errors: RETRYABLE_ERRORS, max: 3, wait: 3.seconds) do
+        Ferrum::Browser
+          .new(headless: "new", browser_options: { "no-sandbox": nil, "disable-setuid-sandbox": nil })
+          .tap do |browser|
+            Rails.logger.info "Browser instance created PID: #{browser.process.pid}"
+          end
+      end
+  end
 end
