1c5642244678ae597a4d68cf904b400924f3493b
Anderson Macedo <anderson.krs95@gmail.com>
2024-02-17 17:32:47 -0300

    Partially move jobs to SolidQueue

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   if Rails.env.production?
     http_basic_authenticate_with(
       name: "admin",
-      password: Rails.application.credentials.dig(:admin, :password)
+      password: Rails.application.credentials.dig(:admin, :password) || ""
     )
   end
 end
