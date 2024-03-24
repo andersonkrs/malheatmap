6b725523fbbe49c1bf08303e09ff89fdce1f3023
Anderson Macedo <anderson.krs95@gmail.com>
2024-03-24 17:06:40 -0300

    Move servers

--- test/jobs/user/periodic_mal_sync_job_test.rb
+++ test/jobs/user/periodic_mal_sync_job_test.rb
@@ -21,30 +21,6 @@ class User::PeriodicMALSyncJobTest < ActiveJob::TestCase
     User::PeriodicMALSyncJob.perform_now(user)
   end
 
-  test "does not enqueue duplicated jobs for the same user version" do
-    user = User.create!(username: "john-bass", mal_synced_at: nil)
-
-    assert_enqueued_jobs 1, only: User::PeriodicMALSyncJob do
-      User::PeriodicMALSyncJob.perform_later(user)
-    end
-
-    assert_no_enqueued_jobs only: User::PeriodicMALSyncJob do
-      User::PeriodicMALSyncJob.perform_later(user)
-    end
-  end
-
-  test "ignores enqueuing limiting when the key is invalidated" do
-    user = User.create!(username: "john-bass", mal_synced_at: Time.current)
-    User::PeriodicMALSyncJob.perform_later(user)
-
-    travel_to 2.minutes.from_now
-    user.update_columns(mal_synced_at: Time.current)
-
-    assert_enqueued_jobs 1, only: User::PeriodicMALSyncJob do
-      User::PeriodicMALSyncJob.perform_later(user)
-    end
-  end
-
   test "enqueues user deactivation when the retries are exhausted and the user is a legacy account" do
     user = User.create!(username: "john-bass", mal_synced_at: nil, mal_id: nil)
 
@@ -53,13 +29,11 @@ class User::PeriodicMALSyncJobTest < ActiveJob::TestCase
     fake_pipeline
       .stubs(:execute!)
       .raises(MAL::Errors::UnableToNavigateToHistoryPage.new(body: "body!", uri: "uri!"))
-      .twice
+      .times(2)
 
     User.any_instance.stubs(:crawler_pipeline).returns(fake_pipeline)
 
     perform_enqueued_jobs only: User::PeriodicMALSyncJob do
-      User::PeriodicMALSyncJob.perform_later(user)
-      User::PeriodicMALSyncJob.perform_later(user)
       User::PeriodicMALSyncJob.perform_later(user)
     end
 
