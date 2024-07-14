443cb13b63af2a415b083f0047f12b074a098694
Anderson Macedo <anderson.krs95@gmail.com>
2024-07-14 08:23:33 -0300

    Increase attemps for generating images

--- app/models/user/signaturable/signature_image.rb
+++ app/models/user/signaturable/signature_image.rb
@@ -26,9 +26,9 @@ class User
       end
 
       class GenerateJob < ApplicationJob
-        retry_on(*BrowserSession::RETRYABLE_ERRORS, wait: :polynomially_longer)
+        retry_on(*BrowserSession::RETRYABLE_ERRORS, attempts: 10, wait: :polynomially_longer)
 
-        limits_concurrency to: 2, key: :screenshots, duration: 2.hours
+        limits_concurrency to: 1, key: :screenshots, duration: 2.hours
 
         queue_as :screenshots
 
