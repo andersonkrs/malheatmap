50f15af1a842e1b9e75790b13ac5976a01793c3b
Anderson Macedo <anderson.krs95@gmail.com>
2024-10-09 18:33:06 -0300

    Add latency helper to solid queue

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,3 +1,20 @@
 Rails.application.configure do
   config.solid_queue.silence_polling = true
 end
+
+require "solid_queue"
+
+module QueueExtensions
+  extend ActiveSupport::Concern
+
+  def latency
+    now = Time.current
+    oldest_job = SolidQueue::ReadyExecution.queued_as(name).minimum(:created_at) || now
+
+    now - oldest_job
+  end
+end
+
+Rails.configuration.to_prepare do
+  SolidQueue::Queue.include(QueueExtensions)
+end
